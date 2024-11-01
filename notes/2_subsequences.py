c319f3d30f6f2c020ca5e8cfd415f84533ed401a
avnish <yadav.tara.avnish@gmail.com>
2024-11-01 16:31:42 +0530

    Updated

--- notes/2_subsequences.py
+++ notes/2_subsequences.py
@@ -1,7 +1,18 @@
 import copy
 
+def solve_using_bit(numbers):
+    n = len(numbers)
+    total_subsets = 1<<n  #2^n using bit wise operation
+    ans = []
+    for ith_subset in range(1,total_subsets):
+        output = []
+        for i in range(n):
+            if ith_subset & (1<<i):
+                output.append(numbers[i])
+        ans.append(output)
+    return ans
 
-    
+
 def solve(numbers,idx,output,ans):
     if idx>=len(numbers):
         if len(output)>0:
@@ -17,5 +28,5 @@ if __name__=="__main__":
     arr = [1,2,3]
     ans=[]
     output=[]
-    solve(arr,0,output,ans)
-    print(ans)
+    #solve(arr,0,output,ans)
+    print(solve_using_bit(arr))
